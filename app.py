acb3f173dd1ff40ebaa7578f4a8d4f98b1534e90
Xin <tankaixin02@gmail.com>
2024-11-13 15:01:13 +0800

    create some page and navigate code in app.py

--- app.py
+++ app.py
@@ -10,6 +10,22 @@ def homepage():
 def route():
     return render_template('Public/route.html')
 
+@app.route('/Public/public_register')
+def route():
+    return render_template('Public/public_register.html')
+
+@app.route('/Public/student_register')
+def route():
+    return render_template('Public/student_register.html')
+
+@app.route('/Public/packages')
+def route():
+    return render_template('Public/packages.html')
+
+@app.route('/Public/about_us')
+def route():
+    return render_template('Public/about_us.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
 
