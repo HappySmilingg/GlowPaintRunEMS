e2b93d91f197ccd1fe2f807d224bcd496035e60b
Xin <tankaixin02@gmail.com>
2024-11-13 16:01:05 +0800

    error solved

--- app.py
+++ app.py
@@ -11,19 +11,19 @@ def route():
     return render_template('Public/route.html')
 
 @app.route('/Public/public_register')
-def route():
+def public_register():
     return render_template('Public/public_register.html')
 
 @app.route('/Public/student_register')
-def route():
+def student_route():
     return render_template('Public/student_register.html')
 
 @app.route('/Public/packages')
-def route():
+def packages():
     return render_template('Public/packages.html')
 
 @app.route('/Public/about_us')
-def route():
+def about_us():
     return render_template('Public/about_us.html')
 
 if __name__ == '__main__':
