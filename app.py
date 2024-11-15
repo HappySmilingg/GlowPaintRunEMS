5ef1ca07138f2c31cad2530b412404b291618bc7
Xin <tankaixin02@gmail.com>
2024-11-15 21:35:34 +0800

    payment update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -15,8 +15,52 @@ def public_register():
     return render_template('Public/public_register.html')
 
 @app.route('/Public/student_register')
-def student_route():
+def student_register():
     return render_template('Public/student_register.html')
+
+@app.route('/submit-form', methods=['POST'])
+def submit_form():
+    if request.method == 'POST':
+        package = request.form['package-details']
+        t_shirt_size = request.form['t-shirt-size']
+
+        return redirect(url_for('payment', package=package, t_shirt_size=t_shirt_size))
+    return render_template('Public/student_register.html')
+
+@app.route('/submit-form2', methods=['POST'])
+def submit_form2():
+    if request.method == 'POST':
+        package = 'Glowrious Pro'
+        t_shirt_size = request.form['t-shirt-size']
+
+        return redirect(url_for('payment', package=package, t_shirt_size=t_shirt_size))
+    return render_template('Public/student_register.html')
+
+@app.route('/Public/payment')
+def payment():
+    package = request.args.get('package')
+    t_shirt_size = request.args.get('t_shirt_size')
+
+    package_price = {
+        'Glowrious Pro': 50,
+        'Glowrious Lite': 35,
+        'Glowrious Starter': 15
+    }
+    
+    additional_price = {
+        '3XL': 3,  
+        '4XL': 5,
+        '5XL': 7,  
+        '6XL': 9,  
+        '7XL': 11
+    }
+    
+    package_price = package_price.get(package, 0)
+    additional_price = additional_price.get(t_shirt_size, 0)
+    
+    total_amount = package_price + additional_price
+
+    return render_template('Public/payment.html', package=package, t_shirt_size=t_shirt_size, package_price=package_price, additional_price=additional_price, total_amount=total_amount)
 
 @app.route('/Public/packages')
 def packages():
